c5d4ed225944c5ab12fe3e7ca77676228e073bf0
slubowsky <270589+slubowsky@users.noreply.github.com>
2023-10-09 21:34:01 -0400

    sl comments on tests

--- JavaScript/72/quiz-map.js
+++ JavaScript/72/quiz-map.js
@@ -1,3 +1,4 @@
+// SL - parens on left side of variable being assigned result of IIFE is unusual. Parens would usually be after the =
 (window.app1 = function () {
     'use strict';
 
@@ -11,6 +12,9 @@
         console.log(`Original Array: ${array}`);
         console.log(`New Array: ${newArray}`);
     }
+
+    // SL - Why not just return the function directly?
+    // For that matter, why not just window.app1 = function(array, func) { ...map impl here... }m you not using the "private" area we gain by having it in an IIFE anyway
     return { myMap };
 
 }());
@@ -19,5 +23,4 @@ const x = [2, 4, 6];
 window.app1.myMap(x, x => x * 2);
 window.app1.myMap(x, x => x + 'test');
 
-
-
+// SL - nice!
--- JavaScript/72/tester.js
+++ JavaScript/72/tester.js
@@ -5,6 +5,7 @@ const counter1 = window.app.counter;
 const counter2 = window.app.createCounter();
 const counter3 = window.app.createCounter();
 
+// SL - not sure a counter is supposed to work with more then 1 increment each time, but no problem
 counter1.increment(10);
 counter2.increment(5);
 counter3.increment(15);
@@ -12,3 +13,6 @@ counter3.increment(15);
 counter1.getCount();
 counter2.getCount();
 counter3.getCount();
+
+// SL - nice!
+// Grade - 100
